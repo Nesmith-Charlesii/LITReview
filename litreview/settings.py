84cfe61bccf20468127568cf64a55e9762f41d05
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2025-08-22 17:29:48 -0400

    project level urls created

--- litreview/settings.py
+++ litreview/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
