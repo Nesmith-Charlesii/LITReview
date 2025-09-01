f12d573f86079b2905b44dc08e31d26dbc7091fd
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2025-08-31 21:01:59 -0400

    updated urls and views to include login and logout

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,32 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")  # Redirect to a home page or dashboard after registration
+    else:
+        form = UserCreationForm() # What does this return by default without any arguments?
+    return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+        else:
+            form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
