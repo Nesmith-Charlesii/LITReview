9f02634c101b4116bf43bedca3acd182b2b508b9
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2025-09-06 16:28:46 -0400

    Sign up form works and adds users to db. CustomUsCreationForm created. Updated views form

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
@@ -9,26 +9,32 @@ def home(request):
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("home")  # Redirect to a home page or dashboard after registration
     else:
-        form = UserCreationForm() # What does this return by default without any arguments?
+        form = CustomUserCreationForm() # What does this return by default without any arguments?
     return render(request, "users/register.html", {"form": form})
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect("home")
-        else:
-            form = AuthenticationForm()
+    else:
+        form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form})
 
 def logout_view(request):
