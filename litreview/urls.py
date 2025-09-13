c0bc36ecf4567217b3b69f50a10cae7d7d0c0ffe
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2025-09-13 18:48:12 -0400

    review edit page set up. needs adjustments for the form

--- litreview/urls.py
+++ litreview/urls.py
@@ -21,9 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
-    path("", include("reviews.urls")),
-    path("reviews/", include("reviews.urls")),
-    path("books/", include("reviews.urls"))
+    path("books/", include("reviews.urls")),
 ]
 
 if settings.DEBUG:  # only in development
--- reviews/urls.py
+++ reviews/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("", views.home, name="home"),
-    path("books/<int:pk>/", views.book_detail, name="book_view"),
-    path('review/<int:pk>/edit/', views.edit_review, name='edit_review'),
+    path("<int:pk>/", views.book_detail, name="book_view"),
+    path('reviews/<int:pk>/edit/', views.edit_review, name='edit_review'),
     path('review/<int:pk>/delete/', views.delete_review, name='delete_review'),
 ]
